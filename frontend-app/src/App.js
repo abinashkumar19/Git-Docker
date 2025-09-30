838d8dec63593a498c3033798ae638094a9cb041
Abinash kumar <163131540+abinashkumar19@users.noreply.github.com>
2025-09-30 15:22:42 +0530

    Update App.js

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function App() {
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Hello from React Frontend 🚀</h1>
-      <p>This app is running inside a Docker container on EC2!</p>
+    <div>
+      <h1>Hello from React App 🚀</h1>
+      <p>Deployed with GitHub Actions, Docker, ECR & EC2</p>
     </div>
   );
 }
