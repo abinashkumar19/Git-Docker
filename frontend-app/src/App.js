36601b966db56496d6ef0577f5b1c4b16079534c
Abinash kumar <163131540+abinashkumar19@users.noreply.github.com>
2025-09-30 15:03:06 +0530

    Create App.js

---
+++ frontend-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello from React Frontend 🚀</h1>
+      <p>This app is running inside a Docker container on EC2!</p>
+    </div>
+  );
+}
+
+export default App;
